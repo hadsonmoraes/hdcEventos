687c01ad0e4b3b973ef1ee35a4348823a5e7ad6a
Hadson Moraes <hadsonmartins10@gmail.com>
2024-06-04 17:26:50 -0400

    tela inicial

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,23 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     public function index()
     {
+        $events = Event::all();
 
-        $nome = "Hadson";
-        $idade = 29;
-        $arr = [10, 20, 30, 40, 50];
-        $nomes = ["Hadson", "Matheus", "Hellen", "Saulo"];
-        return view('welcome', [
-            'nome' => $nome,
-            'idade' => $idade,
-            'profissao' => "Programador",
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]);
+        return view('welcome', ['events' => $events]);
     }
 
     public function create()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,34 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
 @section('title', 'HDC Events')
     
 @section('content')
+
+
+<div class="col-md-12" id="search-container">
+    <h1>Busque um evento</h1>
+    <form action="" method="post">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($events as $event)
+ <div class="card col-md-3">
+    <img src="/img/event_placeholder.jpg" alt="{{$event->title}}">
+    <div class="card-body">
+        <p class="card-date">04/06/2024</p>
+        <h5 class="card-title">{{$event->title}}</h5>
+        <p class="card-participants">X Participantes</p>
+        <a href="#" class="btn btn-primary">Saber mais</a>
+    </div>
+ </div>
+    @endforeach
+    </div>
+</div>
+
+
 
 @endsection
