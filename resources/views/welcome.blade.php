f09d9229493dff0f42841b4835e3802e5c9d3450
Hadson Moraes <hadsonmartins10@gmail.com>
2024-06-04 10:58:37 -0400

    remover mais testes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,38 +4,4 @@
     
 @section('content')
 
-<h1>Hello World</h1>
-    <img src="img/banner.jpg" alt="Banner">
-    @if(10 > 15)
-    <p>A condição é true</p>
-    @endif
-
-    <p> {{$nome}} </p>
-
-    @if($nome == "Pedro")
-    <p>O nome é Pedro</p>
-    @elseif($nome == "Hadson")
-    <p>O nome é {{ $nome }} e ele tem {{ $idade }} anos</p>
-    @else
-    <p>O nome não é Pedro</p>
-    @endif
-
-    @for($i = 0; $i < count($arr); $i++)
-    <p> {{$arr[$i]}} - {{$i}} </p>
-    @if ($i == 2 )
-        <p>O I é 2</p>
-    @endif
-    @endfor
-
-    @foreach ($nomes as $nome)
-    <p>{{$loop->index}}  </p>
-        <p> {{$nome}} </p>
-    @endforeach
-
-{{-- *textando extensão do comentario --}}
-    @php
-        $name = "João"; 
-        echo $name;
-    @endphp
-
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,3 @@ Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/contact', function () {
     return view('contact');
 });
-
-Route::get('/produtos', function () {
-    $busca = request('search');
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos/{id}', function ($id) {
-    return view('product', ['id' => $id]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});
