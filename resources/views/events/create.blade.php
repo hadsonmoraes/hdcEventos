ed3c39566bdae421991eebe09361aa234bcb6dd1
Hadson Moraes <hadsonmartins10@gmail.com>
2024-06-04 18:04:26 -0400

    criar evento

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,9 +1,34 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
 @section('title', 'Produtos')
     
 @section('content')
 
-<h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+  <h1>Crie o seu evento</h1>
+  <form action="/events" method="post">
+    @csrf
+    <div class="mb-3">
+      <label for="title" class="form-label">Evento:</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+    </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">Cidade:</label>
+      <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+    </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">O evento é privado ?</label>
+<select name="private" id="private" class="form-control">
+  <option value="0">Não</option>
+  <option value="1">Sim</option>
+</select>
+    </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">Descrição do evento:</label>
+     <textarea name="description" id="description"  class="form-control" placeholder="O que vai acontecer no evento ?"></textarea>
+    </div>
+    <input type="submit" class="btn btn-primary" value="Criar Evento">
+  </form>
+</div>
 
 @endsection
