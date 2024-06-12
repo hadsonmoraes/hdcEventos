0298c7e7beba76e9fac4cab3928ddb598094c072
Hadson Moraes <hadsonmartins10@gmail.com>
2024-06-12 17:58:07 -0400

    Relação One to Many

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -79,7 +79,7 @@
     @yield('content')
 
     <footer>
-        <p>HDC Events &copy; 2024</p>
+        <p>HDC Events &copy; {{ date('Y') }}</p>
     </footer>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
